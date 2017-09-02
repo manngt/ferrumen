ca7189ff51d4959350cddab89e5249ae6c4d7079
MAUS502 <manngt@gmail.com>
2017-09-02 10:44:03 -0600

    fix lenght key

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -15,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     {
         
         Schema::defaultStringLength(191);
-        
+
     }
 
     /**
